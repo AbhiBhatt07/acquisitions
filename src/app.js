5d01310e86050147c1d3fe9693c68343f69fa62b
Abhishek Bhatt <abhibhatt587@gmail.com>
2025-12-02 00:21:22 +0530

    make api's and test those api on Httpie and everything is working well.

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import router from '#routes/auth.routes.js';
 
 const app = express();
 app.use(helmet());
@@ -22,5 +23,21 @@ app.get('/', (req, res) => {
   logger.info('Hello from acquisitions!');
   res.status(200).send('Hello from acquisitions!!!!');
 });
+
+app.get('/health', (req, res) => {
+  res.status(200).json({
+    status: 'OK !',
+    timestamp: new Date().toISOString(),
+    uptime: process.uptime(),
+  });
+});
+
+app.get('/api', (req, res) => {
+  res.status(200).json({
+    message: 'Acquisitions API is running!',
+  });
+});
+
+app.use('/api/auth', router);
 
 export default app;
